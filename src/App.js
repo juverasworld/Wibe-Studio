3af7d67f53a96ceaaf7ca5c3fd2ecc05910b6047
Vee <ifebuchejulia@gmail.com>
2024-02-27 12:42:42 +0100

    added the locomotive package

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
-import GlobalStyles from './styles/GlobalStyles';
-
+import { ThemeProvider } from "styled-components";
+import logo from "./logo.svg";
+import GlobalStyles from "./styles/GlobalStyles";
+import { dark } from "./styles/Themes";
+import { LocomotiveScrollProvider } from "react-locomotive-scroll";
+import { useRef } from "react";
 
 function App() {
+  const containerRef = useRef(null);
   return (
     <>
-    <GlobalStyles/>
-    APp File
+      <GlobalStyles />
+      <ThemeProvider theme={dark}>
+        <LocomotiveScrollProvider
+          options={{
+            smooth: true,
+            // ... all available Locomotive Scroll instance options
+          }}
+          watch={
+            [
+              //..all the dependencies you want to watch to update the scroll.
+              //  Basicaly, you would want to watch page/location changes
+              //  For exemple, on Next.js you would want to watch properties like `router.asPath` (you may want to add more criterias if the instance should be update on locations with query parameters)
+            ]
+          }
+          containerRef={containerRef}
+        >
+          <main data-scroll-container ref={containerRef}>
+            App File
+          </main>
+        </LocomotiveScrollProvider>
+      </ThemeProvider>
     </>
   );
 }
---
+++ src/styles/Themes.js
@@ -0,0 +1,18 @@
+export const dark = {
+  body: "#202020", //black shade
+  text: "#fff",
+  bodyRgba: "32,32,32",
+  textRgba: "255, 2555, 2555",
+
+  grey: "#bebebe",
+  fontxs: "0.75em",
+  fontsm: "0.875em",
+  fontmd: "1em",
+  fontlg: "1.25em",
+  fontxl: "2em",
+  fontxxl: "3em",
+  fontxxl: "5em",
+  fontBig: "10em",
+
+  navHeight: "5rem",
+};
