30d35d942f81757178082306a1b4f3bc5c7f7e61
Vee <ifebuchejulia@gmail.com>
2024-02-29 05:04:46 +0100

    completed tha about us

--- src/section/About.jsx
+++ src/section/About.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import styled from "styled-components";
+import img1 from "../assets/Images/1.webp";
+import img2 from "../assets/Images/2.webp";
+import img3 from "../assets/Images/3.webp";
 
 const Section = styled.section`
   position: relative;
   min-height: 100vh;
   width: 80vw;
-  overflow: hidden;
+  display: flex;
+
+  margin: 0 auto;
 `;
 
 const Title = styled.h1`
@@ -19,9 +24,39 @@ const Title = styled.h1`
   z-index: 5;
 `;
 
+const Left = styled.div`
+  width: 50%;
+  font-size: ${(props) => props.theme.fontlg};
+  font-weight: 300;
+  position: relative;
+  z-index: 5;
+  margin-top: 20%;
+`;
+const Right = styled.div`
+  width: 50%;
+  position: relative;
+
+  img {
+    width: 100%;
+    height: auto;
+  }
+  .small-img-1 {
+    width: 48%;
+    position: absolute;
+    right: 95%;
+    bottom: 10%;
+  }
+  .small-img-2 {
+    width: 48%;
+    position: absolute;
+    left: 80%;
+    bottom: 30%;
+  }
+`;
+
 const About = () => {
   return (
-    <Section>
+    <Section id="fixed-target">
       <Title
         data-scroll
         data-scroll-speed="-2"
@@ -29,6 +64,34 @@ const About = () => {
       >
         About Us
       </Title>
+      <Left data-scroll data-scroll-sticky data-scroll-target="#fixed-target">
+        We're fashion studio based in Aba. We create unique designs that will
+        blow your mind. We also design unique jewellary pieces. Fashion is an
+        ART that can not be grasped by everyone.
+        <br />
+        <br />
+        We are very dedicated to making our products. We offer unique and
+        creative products to a wide range of people. We have a variety of
+        styles, but for most people, all of the options are in the box. We
+        specialize in making things that make you happy.
+        <br />
+        <br />
+        We strive to build on our vision. As a fashion label, we do our best to
+        create amazing experiences for all people. We are always looking to make
+        something that is easy for everyone.
+      </Left>
+      <Right>
+        <img
+        data-scroll
+        data-scroll-speed="5"
+        
+         src={img1} alt="Abot us " />
+        <img
+          data-scroll
+          data-scroll-speed="-2"
+         src={img2} className="small-img-1" alt="Abot us " />
+        <img src={img3} className="small-img-2" alt="Abot us " />
+      </Right>
     </Section>
   );
 };
